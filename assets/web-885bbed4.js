bed53e45e6085ebc185ad65fb5196a0417bfccca
petroskayange <kayangepetros@gmail.com>
2024-02-22 10:13:14 +0200

    adding mahis base url

---
+++ assets/web-885bbed4.js
@@ -0,0 +1,1 @@
+import{W as n,A as t}from"./index-4b713d9c.js";class l extends n{async write(e){if(e.url)return t.expandPath(e.url).then(r=>{document.location=r});throw new TypeError("Undefined url")}async discover(){return{devices:["webPrinter"]}}}export{l as LabelPrinterWeb};
