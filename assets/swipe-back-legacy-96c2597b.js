6d20fdbdcc01bd7040e685601e7986740602440e
petroskayange <kayangepetros@gmail.com>
2024-04-24 12:23:34 +0200

    releasing new changes

--- assets/swipe-back-legacy-96c2597b.js
+++ assets/swipe-back-legacy-96c2597b.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-a87a717f.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.y,r=t.z,i=t.A}],execute:function(){
+System.register(["./index-legacy-5be0c8c8.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.y,r=t.z,i=t.A}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
