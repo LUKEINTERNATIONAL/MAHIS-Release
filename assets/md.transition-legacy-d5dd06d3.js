1175ef5175cb6671062a58d222a8be69d2ed5988
petroskayange <kayangepetros@gmail.com>
2024-05-14 03:59:51 +0200

    new realse

--- assets/md.transition-legacy-d5dd06d3.js
+++ assets/md.transition-legacy-d5dd06d3.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-1696997a.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.C,i=e.B}],execute:function(){
+System.register(["./index-legacy-fa1ae26e.js"],(function(e,n){"use strict";var t,i;return{setters:[e=>{t=e.C,i=e.B}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
