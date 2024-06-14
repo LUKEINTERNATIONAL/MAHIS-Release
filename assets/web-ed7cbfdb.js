18be24835e2c1506087148d60cd31df4d9eb2665
petroskayange <kayangepetros@gmail.com>
2024-06-14 16:24:46 +0200

    new release

---
+++ assets/web-ed7cbfdb.js
@@ -0,0 +1,1 @@
+import{W as n,U as t}from"./index-4ec03a19.js";class l extends n{async write(e){if(e.url)return t.expandPath(e.url).then(r=>{document.location=r});throw new TypeError("Undefined url")}async discover(){return{devices:["webPrinter"]}}}export{l as LabelPrinterWeb};
